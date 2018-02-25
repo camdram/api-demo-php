62a2e885a0031eda3afb305f5d410d3c4eb173ef
Peter Hoyes <pahoyes@gmail.com>
2018-02-25 14:03:03 +0000

    Remove subfolders

--- php/config.sample.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Create an API key at https://www.camdram.net/api/apps
-
-define('CAMDRAM_URL', 'http://www.camdram.net');
-define('API_KEY', '???????');
-define('API_SECRET', '???????');
--- python/config.example.py
+++
@@ -1,5 +0,0 @@
-#Copy this file to config.py and fill in values
-#Create an API key at https://www.camdram.net/api/apps
-CAMDRAM_URL = 'http://www.camdram.net'
-API_KEY = '??????'
-API_SECRET = '??????'
